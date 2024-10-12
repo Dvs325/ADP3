5376386009f29fa628ac6165139bdec2a95c1c1e
alasd <alasd6411@gmail.com>
2024-10-11 21:03:40 -0600

    php

---
+++ Config/Config - copia (2).php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "mysql://urkbw393ryljjvkb:7K7iNRNVVd10K0a4cpaB@bn4uazxfpyzabqc0bs4d-mysql.services.clever-cloud.com:3306/bn4uazxfpyzabqc0bs4d";
+const host = "bi9vdxh56wuyux2gzxpi-mysql.services.clever-cloud.com";
+const user = "urkbw393ryljjvkb";
+const pass = "7K7iNRNVVd10K0a4cpaB";
+const db = "bn4uazxfpyzabqc0bs4d";
+const charset = "charset=utf8";
+?>
--- Config/Config1.php
+++ Config/Config1.php
@@ -1,8 +1,8 @@
 <?php
-const base_url = "http://adp.free.nf/biblio/index.php";
+const base_url = "mysql://uupfymbhq6dwiqx0:eZZxisMqDLQIrZpObU0z@bi9vdxh56wuyux2gzxpi-mysql.services.clever-cloud.com:3306/bi9vdxh56wuyux2gzxpi";
 const host = "bi9vdxh56wuyux2gzxpi-mysql.services.clever-cloud.com";
 const user = "uupfymbhq6dwiqx0";
 const pass = "eZZxisMqDLQIrZpObU0z";
 const db = "bi9vdxh56wuyux2gzxpi";
 const charset = "charset=utf8";
-?>
+?> 
